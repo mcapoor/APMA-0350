08798dca96b6836b8a52603c9b3bbbbe59776268
Milan Capoor <40697079+mcapoor@users.noreply.github.com>
2024-12-12 11:19:58 -0500

    Reorganize repo

--- dsolve.py
+++
@@ -1,9 +0,0 @@
-from sympy import *
-
-t = symbols('t')
-y = Function('y')
-deq = (t * diff(y(t), t)) + ((t + 1) * y(t)) - (2 * t * exp(-t)) 
-ysoln = dsolve(deq, y(t), ics={y(1): 2})
-
-print(ysoln)
-plot(ysoln.rhs,(t, -5, 5), ylim=[-20, 2])
